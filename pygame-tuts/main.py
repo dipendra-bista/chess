74f41367dc336cf29f8641e8607d92ffcd71f77e
punoj <punojbista44@gmail.com>
2020-07-26 14:57:20 +0545

    rectangular is moving but storing previous frame and also time transition is so fast we can no see the transaction

--- pygame-tuts/main.py
+++ pygame-tuts/main.py
@@ -7,7 +7,11 @@ done = False
 # setting title of the window
 pygame.display.set_caption("Chess Game")
 # setting height and width of the window
-pygame.display.set_mode((400, 400))
+screen = pygame.display.set_mode((400, 400))
+
+x = 30
+y = 30
+is_blue = True
 ''' 
 By default windows are not display forever so to make
 it display untill user press close event
@@ -16,5 +20,20 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-
-pygame.display.flip()
+        if event.type == pygame.KEYDOWN and event.type == pygame.K_SPACE:
+            is_blue = not is_blue
+    pressed = pygame.key.get_pressed()
+    if pressed[pygame.K_UP]:
+        y -= 3
+    if pressed[pygame.K_DOWN]:
+        y += 3
+    if pressed[pygame.K_LEFT]:
+        x -= 3
+    if pressed[pygame.K_RIGHT]:
+        x += 3
+    if is_blue:
+        color = [0, 128, 255]
+    else:
+        color = [255, 100, 0]
+    pygame.draw.rect(screen, color, pygame.Rect(x, y, 60, 60))
+    pygame.display.flip()
