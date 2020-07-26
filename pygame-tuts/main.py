aab8cd9fe8fd8d99156a34e3602eb91895db5315
punoj <punojbista44@gmail.com>
2020-07-26 14:44:41 +0545

    pygame helloworld

---
+++ pygame-tuts/main.py
@@ -0,0 +1,20 @@
+import pygame
+
+# initialize all the pygame modules
+pygame.init()
+# Set not to exit the window
+done = False
+# setting title of the window
+pygame.display.set_caption("Chess Game")
+# setting height and width of the window
+pygame.display.set_mode((400, 400))
+''' 
+By default windows are not display forever so to make
+it display untill user press close event
+'''
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+pygame.display.flip()
